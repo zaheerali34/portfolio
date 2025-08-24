f6cbcfba455cb0db674f79de392432d14aad2ff2
Zaheer Ali <zaheerrahman0@gmail.com>
2025-08-24 23:23:47 +0500

    updata

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -6,7 +6,9 @@ function Home() {
     <div className='w-full'>
         <h1 className='text-white font-bold text-3xl mt-5'>Hi, I'm Zaheer Rahman</h1>
         <h3 className='text-zinc-300 font-semibold text-xl mt-3'>Frontend Developer | From Problem to Production-Ready, at Scale</h3>
-        <a href="https://drive.google.com/file/d/1VPRtkSCDoT1BDmq_eV6gy1ZadSYJKdSS/view">
+        <a 
+        target="_blank" 
+        href="https://drive.google.com/file/d/1qDvyv2ju6LXqLmXPKBfRNfp6e2DdGYGy/view">
           <button className='mt-5 border-1 border-zinc-400 text-blue-500 font-semibold hover:text-blue-600 transition-all duration-300 cursor-pointer py-2 px-5 rounded-full'><i class="ri-drive-line"></i> Resume, CV</button>
         </a>
     </div>
